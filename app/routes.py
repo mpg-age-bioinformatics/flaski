647a1a9976fa56deff59fea1346ae7d573e527e5
jorgeboucas <mail@jorgeboucas.com>
2019-12-11 13:57:57 +0100

    advanced working template

--- app/routes.py
+++ app/routes.py
@@ -1,18 +1,45 @@
-from flask import render_template
+from flask import render_template, Flask, Response, request
 from app import app
+
+import io
+import random
+
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_svg import FigureCanvasSVG
+from matplotlib.figure import Figure
+
+@app.route("/matplot-as-image.png")
+def plot_png(num_x_points=50, linewidth=1):
+    """ 
+    renders the plot on the fly.
+    https://gist.github.com/illume/1f19a2cf9f26425b1761b63d9506331f
+
+    """
+    fig = Figure(figsize=(20,20))
+    axis = fig.add_subplot(1, 1, 1)
+    x_points = range(num_x_points)
+    axis.plot(x_points, [random.randint(1, 30) for x in x_points] )
+    output = io.BytesIO()
+    FigureCanvasAgg(fig).print_png(output)
+    return Response(output.getvalue(), mimetype="image/png")
+
+@app.route("/matplot-as-image.svg")
+def plot_svg(num_x_points=50):
+    """ 
+    renders the plot on the fly.
+    https://gist.github.com/illume/1f19a2cf9f26425b1761b63d9506331f
+    """
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    x_points = range(num_x_points)
+    axis.plot(x_points, [random.randint(1, 30) for x in x_points])
+    output = io.BytesIO()
+    FigureCanvasSVG(fig).print_svg(output)
+    return Response(output.getvalue(), mimetype="image/svg+xml")
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    plot_png()
+    return render_template('index.html', figure_url ='/matplot-as-image.png')
