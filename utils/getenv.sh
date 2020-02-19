a45fd29555c76dcd5794aaea854e9f00ca55f975
jorgeboucas <mail@jorgeboucas.com>
2020-02-19 12:36:22 +0100

    new reqs

--- utils/getenv.sh
+++ utils/getenv.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #pip3 list | egrep -v 'flaski|six|pip' | sed '1,2d' | awk '{ print $1"=="$2 }' | grep cython
-echo "cython"
+#echo "cython"
 pip3 list | egrep -v 'flaski|six|pip' | sed '1,2d' | awk '{ print $1"=="$2 }' | grep numpy
 pip3 list | egrep -v 'flaski|six|pip' | sed '1,2d' | awk '{ print $1"=="$2 }' | grep -v numpy
