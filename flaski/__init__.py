fa8de2950153d3b47af68eefc953439da6d3876d
jorgeboucas <mail@jorgeboucas.com>
2020-05-08 09:28:16 +0200

    __init__.py: debuger emails with correct from address

--- flaski/__init__.py
+++ flaski/__init__.py
@@ -36,7 +36,7 @@ if not app.debug:
             secure = ()
         mail_handler = SMTPHandler(
             mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+            fromaddr=app.config['ADMINS'][0],
             toaddrs=app.config['ADMINS'], subject='Flaski Failure',
             credentials=auth, secure=secure)
         mail_handler.setLevel(logging.ERROR)
