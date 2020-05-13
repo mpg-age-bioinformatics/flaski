fa355d9cb898e94e98d693fc7677bb113a7f6030
jorgeboucas <mail@jorgeboucas.com>
2020-05-13 18:53:04 +0200

    utils/dev.push.sh

--- utils/dev.push.sh
+++ utils/dev.push.sh
@@ -2,4 +2,4 @@
 
 git add -A . 
 git commit -m "$1"
-docker build -t flaski/flaski:dev . && docker push flaski/flaski:dev && curl -X POST https://portainer.age.mpg.de/api/webhooks/304d48ea-8495-497e-933d-49c8be2f2ef7
+docker build -t flaski/flaski:dev . && docker push flaski/flaski:dev && sleep 5 && curl -X POST https://portainer.age.mpg.de/api/webhooks/304d48ea-8495-497e-933d-49c8be2f2ef7
