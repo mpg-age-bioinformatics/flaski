36269335d4f15d47e6fbd815b6d2307cf0846589
jorgeboucas <mail@jorgeboucas.com>
2020-05-16 08:16:14 +0200

    deploy: typo

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -52,7 +52,7 @@ fi
 if [[ "$1" == "down" ]] ; 
     then
 
-        docker-compose -f production.yml exec backup /backup.sh >  && \
+        docker-compose -f production.yml exec backup /backup.sh && \
         docker-compose -f production.yml exec backup rsync -rtvh --delete /flaski_data/users/ /backup/users_data/ && \
         docker-compose -f production.yml pull server && \
         echo "$(date) :: docker-compose down" && \
