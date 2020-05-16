6a2d173001a75d5d50fac775d6b4235d796bd48b
jorgeboucas <mail@jorgeboucas.com>
2020-05-16 09:27:33 +0200

    deploy

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -59,7 +59,9 @@ for BASE_IMAGE in ${BASE_IMAGES}; do
     #IMAGE="$REGISTRY/$BASE_IMAGE"
     IMAGE=$BASE_IMAGE
     CID=$(docker ps | grep $IMAGE | awk '{print $1}')
+    echo "$(date) :: docker pull $IMAGE started\n####################################"
     docker pull $IMAGE
+    echo "####################################\n$(date) :: docker pull $IMAGE finished\n"
 
     for im in $CID
     do
