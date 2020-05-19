d8b3fe4842e37e9a2017cefbe3590a94831cd321
jorgeboucas <mail@jorgeboucas.com>
2020-05-19 16:01:27 +0200

    deploy.sh: pull failures

--- utils/deploy.sh
+++ utils/deploy.sh
@@ -77,8 +77,14 @@ do
         echo "$(date) :: docker pull $IMAGE started"
         #echo "####################################"
         docker pull $IMAGE  > /dev/null 2>&1
+        while [ $? -ne 0 ] ; do 
+            echo "$(date) :: docker pull $IMAGE failed"
+            echo "$(date) :: sleeping for 1h"
+            sleep 3600
+            docker pull $IMAGE > /dev/null 2>&1
+        done
         #echo "####################################"
-        echo "$(date) :: docker pull $IMAGE finished"
+        echo "$(date) :: docker pull $IMAGE finished"        echo "$(date) :: docker pull $IMAGE finished"
 
         for im in $CID
         do
