1792902778ccbaa1077b8aae5bd74368a4a3a62d
jorgeboucas <mail@jorgeboucas.com>
2020-05-19 15:08:47 +0200

    watch.dev.sh

--- utils/watch.dev.sh
+++ utils/watch.dev.sh
@@ -2,7 +2,56 @@
 
 cd /srv/flaski
 
-while docker-compose -f production.yml up -d server
+while true
 do
+
+    BASE_IMAGES=$(docker ps | grep server | awk '{ print $2 }') 
+    #  flaski/backup:latest redis:5 nginx:alpine mariadb:10.4"
+
+    DEPLOY="0"
+
+    for BASE_IMAGE in ${BASE_IMAGES}; do
+
+        #REGISTRY="registry.hub.docker.com"
+        #IMAGE="$REGISTRY/$BASE_IMAGE"
+        IMAGE=$BASE_IMAGE
+        CID=$(docker ps | grep $IMAGE | awk '{print $1}')
+        echo "$(date) :: docker pull $IMAGE started"
+        echo "####################################"
+        docker pull $IMAGE
+        echo "####################################"
+        echo "$(date) :: docker pull $IMAGE finished"
+
+        for im in $CID
+        do
+            LATEST=`docker inspect --format "{{.Id}}" $IMAGE`
+            RUNNING=`docker inspect --format "{{.Image}}" $im`
+            NAME=`docker inspect --format '{{.Name}}' $im | sed "s/\///g"`
+            echo "$(date) :: Latest:" $LATEST
+            echo "$(date) :: Running:" $RUNNING
+            if [ "$RUNNING" != "$LATEST" ];then
+                echo "$(date) :: $NAME needs upgrading"
+                DEPLOY="1"
+            else
+                echo "$(date) :: $NAME up to date"
+            fi
+        done
+    done
+
+    if [[ "$DEPLOY" == "1" ]] ; then
+
+        # maybe need to add this command after the pull and before the up
+        # `docker-compose -f production.yml stop server &&`
+        
+        docker-compose -f production.yml pull server && \
+        docker-compose -f production.yml up --no-start server && \
+        echo "$(date) :: docker-compose up -d" && \
+        docker-compose -f production.yml start server
+
+    fi
+
+    echo "$(date) :: Done!"
+
+
     sleep 30
 done
