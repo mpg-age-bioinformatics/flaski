c38c91c6ee6f091c2535e3bd69b8f4ffff754cd1
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2020-06-12 13:59:05 +0200

    histogram from Camila

---
+++ flaski/apps/main/histogram.py
@@ -0,0 +1,143 @@
+import pandas as pd
+import numpy as np
+import matplotlib
+import matplotlib.pylab as plt
+import sys
+matplotlib.use('agg')
+
+def GET_COLOR(x):
+    if str(x)[:3].lower() == "rgb":
+        vals=x.split("rgb(")[-1].split(")")[0].split(",")
+        vals=[ float(s.strip(" ")) for s in vals ]
+        #vals=tuple(vals)
+        return vals
+    else:
+        return str(x)
+        
+
+def make_figure(df,pa):
+    
+    tmp=df.copy()
+    tmp=tmp[pa["vals"]]
+
+
+    fig, axes = plt.subplots(1, 1,figsize=(float(pa["fig_width"]),float(pa["fig_height"])))
+    
+#
+#    values=pa["values"].split(",")
+#    values=[ s.rstrip("\r") for s in values ]
+#    values=[ s for s in values if s != "" ]
+    for h in pa["groups_settings"].values():
+        pa_={}
+        if h["color_rgb"] != "":
+            pa_["color_value"] = GET_COLOR( h["color_rgb"] )
+        else:
+            if h["color_value"]=="None":
+                h["color_value"]=None
+                pa_["color_value"] = h["color_value"]
+            else:
+                pa_["color_value"] = h["color_value"]
+                
+        if h[ "line_rgb"] != "":
+            pa_["line_color"] = GET_COLOR( h["line_rgb"] )
+        else:
+            if h["line_color"]=="None":
+                h["line_color"]=None
+                pa_["line_color"] = h["line_color"]
+            else:
+                pa_["line_color"] = h["line_color"]
+
+        if h["bins_number"] != "":
+            pa_["bins_number"] = int(h["bins_number"])
+        else:
+            pa_["bins_number"] = h["bins_value"]
+
+
+        
+        if h[ "label"] != "":
+            pa_["label"] = h["label"]
+        else:
+            pa_["label"] = h["name"]
+            
+        if h["log_scale"] == "on":
+            pa_["log_scale"]=False
+        else:
+            pa_["log_scale"]=True
+            
+        if h["fill_alpha"]!=pa["fill_alpha"]:
+            pa_["fill_alpha"]=float(h["fill_alpha"])
+        else:
+            pa_["fill_alpha"]=float(pa["fill_alpha"])
+        
+        if h["linewidth"]!=pa["linewidth"]:
+            pa_["linewidth"]=float(h["linewidth"])
+        else:
+            pa_["linewidth"]=float(pa["linewidth"])
+            
+        
+        if pa_["line_color"]!=None:
+            plt.hist(x=h["values"],bins=pa_["bins_number"],histtype=h["histtype_value"],orientation=h["orientation_value"],label=pa_["label"],color=pa_["color_value"], alpha=pa_["fill_alpha"],lw=pa_["linewidth"],edgecolor=pa_["line_color"],log=pa_["log_scale"],linestyle=h["linestyle_value"])
+        else:
+            plt.hist(x=h["values"],bins=pa_["bins_number"],histtype=h["histtype_value"],orientation=h["orientation_value"],label=pa_["label"],color=pa_["color_value"], alpha=pa_["fill_alpha"],lw=pa_["linewidth"],log=pa_["log_scale"],linestyle=h["linestyle_value"])
+            
+
+
+    plt.title(pa["title"], fontsize=float(pa["title_size_value"]))
+    plt.legend()
+    locs, labels = plt.xticks()
+    min_loc=min(locs)
+    max_loc=max(locs)
+    step_loc=(max_loc-min_loc)/10
+    min_label=min(tmp.min())
+    max_label=max(tmp.max())
+    step_label=(max_label-min_label)/10
+    plt.xticks(np.arange(min_loc,max_loc,step_loc),np.around(np.arange(min_label,max_label,step_label),2))
+
+
+    return fig
+
+STANDARD_BINS=['auto', 'sturges','fd','doane', 'scott','rice','sqrt']
+STANDARD_SIZES=[ str(i) for i in list(range(101)) ]
+STANDARD_COLORS=[None,"blue","green","red","cyan","magenta","yellow","black","white"]
+LINE_STYLES=["solid","dashed","dashdot","dotted"]
+HIST_TYPES=['bar', 'barstacked', 'step',  'stepfilled']
+#LOG_TYPES=[False,True]
+STANDARD_ORIENTATIONS=['vertical','horizontal']
+STANDARD_ALIGNMENTS=['left','right','mid']
+
+def figure_defaults():
+    plot_arguments={
+        "fig_width":"6.0",\
+        "fig_height":"6.0",\
+        "title":'Histogram',\
+        "title_size":STANDARD_SIZES,\
+        "title_size_value":"20",\
+        "fill_alpha":0.8,\
+        "linewidth":1.0,\
+        "cols":[],\
+        "groups":[],\
+        "vals":[],\
+        "list_of_groups":[],\
+        "groups_settings":dict(),\
+        "show_legend":".on",\
+        "log_scale":".off",\
+        "legend_font_size":"14",\
+        "colors":STANDARD_COLORS,\
+        "bins":STANDARD_BINS,\
+        "alignment":STANDARD_ALIGNMENTS,\
+        "alignment_value":"mid",\
+        "hist_types":HIST_TYPES,\
+        "histtype_value":"bar",\
+        "linestyles":LINE_STYLES,\
+        "linestyle_value":"",\
+        "orientations":STANDARD_ORIENTATIONS, \
+        "download_format":["png","pdf","svg"],\
+        "downloadf":"pdf",\
+        "downloadn":"Histogram",\
+        "session_downloadn":"MySession.histogram.plot",\
+        "inputsessionfile":"Select file..",\
+        "session_argumentsn":"MyArguments.histogram.plot",\
+        "inputargumentsfile":"Select file.."
+    }
+
+    return plot_arguments
---
+++ flaski/apps/routes/histogram.py
@@ -0,0 +1,213 @@
+from flask import render_template, Flask, Response, request, url_for, redirect, session, send_file, flash, jsonify
+from flaski import app
+from werkzeug.utils import secure_filename
+from flask_session import Session
+from flaski.forms import LoginForm
+from flask_login import current_user, login_user, logout_user, login_required
+from datetime import datetime
+from flaski import db
+from werkzeug.urls import url_parse
+from flaski.apps.main.histogram import make_figure, figure_defaults
+from flaski.models import User, UserLogging
+from flaski.routines import session_to_file, check_session_app, handle_exception, read_request, read_tables, allowed_file
+from flaski.routes import FREEAPPS
+from flaski.email import send_exception_email
+
+
+import os
+import io
+import sys
+import random
+import json
+
+import matplotlib
+matplotlib.use('agg')
+from matplotlib.backends.backend_agg import FigureCanvasAgg
+from matplotlib.backends.backend_svg import FigureCanvasSVG
+from matplotlib.figure import Figure
+import matplotlib.pyplot as plt
+
+import pandas as pd
+
+import base64
+
+
+@app.route('/histogram/<download>', methods=['GET', 'POST'])
+@app.route('/histogram', methods=['GET', 'POST'])
+@login_required
+def histogram(download=None):
+
+    apps=current_user.user_apps
+    
+    reset_info=check_session_app(session,"histogram",apps)
+    if reset_info:
+        flash(reset_info,'error')
+        # INITIATE SESSION
+        session["filename"]="Select file.."
+        plot_arguments=figure_defaults()
+        session["plot_arguments"]=plot_arguments
+        session["COMMIT"]=app.config['COMMIT']
+        session["app"]="histogram"
+        
+    """
+    renders the plot on the fly.
+    https://gist.github.com/illume/1f19a2cf9f26425b1761b63d9506331f
+    """
+    if request.method == 'POST' :
+
+        plot_arguments = session["plot_arguments"]
+        
+        try:
+            if request.files["inputsessionfile"] :
+                msg, plot_arguments, error=read_session_file(request.files["inputsessionfile"],"heatmap")
+                if error:
+                    flash(msg,'error')
+                    return render_template('/apps/heatmap.html' , filename=session["filename"],apps=apps, **plot_arguments)
+                flash(msg,"info")
+
+            if request.files["inputargumentsfile"] :
+                msg, plot_arguments, error=read_argument_file(request.files["inputargumentsfile"],"heatmap")
+                if error:
+                    flash(msg,'error')
+                    return render_template('/apps/heatmap.html' , filename=session["filename"], apps=apps, **plot_arguments)
+                flash(msg,"info")
+            
+            # IF THE USER UPLOADS A NEW FILE
+            # THEN UPDATE THE SESSION FILE
+            # READ INPUT FILE
+            inputfile = request.files["inputfile"]
+            if inputfile:
+                filename = secure_filename(inputfile.filename)
+                if allowed_file(inputfile.filename):
+                    df=read_tables(inputfile)
+                    cols=df.columns.tolist()
+
+                    # IF THE USER HAS NOT YET CHOOSEN COLUMNS TO PLOT AS HISTOGRAMS
+                    if "vals" not in session["plot_arguments"].keys():
+
+                        session["plot_arguments"]["cols"]=cols
+                                    
+                        sometext="Please select the columns from which we will plot your histograms"
+                        plot_arguments=session["plot_arguments"]
+                        flash(sometext,'info')
+                        return render_template('/apps/histogram.html' , filename=filename, apps=apps,**plot_arguments)
+                    
+                else:
+                    # IF UPLOADED FILE DOES NOT CONTAIN A VALID EXTENSION PLEASE UPDATE
+                    error_msg="You can can only upload files with the following extensions: 'xlsx', 'tsv', 'csv'. Please make sure the file '%s' \
+                    has the correct format and respective extension and try uploadling it again." %filename
+                    flash(error_msg,'error')
+                    return render_template('/apps/histogram.html' , filename="Select file..", apps=apps, **plot_arguments)
+
+            if "df" not in list(session.keys()):
+                error_msg="Please upload a file with data to plot."
+                flash(error_msg,'error')
+                return render_template('/apps/histogram.html' , filename="Select file..", apps=apps, **plot_arguments)
+            
+            if not request.files["inputsessionfile"] and not request.files["inputargumentsfile"] :
+                plot_arguments=read_request(request)
+                df=pd.read_json(session["df"])
+                cols=df.columns.tolist()
+                
+                #IN CASE THE USER HAS UNSELECTED ALL THE COLUMNS THAT WE NEED TO PLOT THE HISTOGRAMS
+                if request.form.getlist("vals") == []:
+                    filename=session["filename"]
+                    session["plot_arguments"]["cols"]=cols
+                    sometext="Please select the columns from which Flaski should plot your histograms"
+                    plot_arguments=session["plot_arguments"]
+                    flash(sometext,'info')
+                    return render_template('/apps/histogram.html' , filename=filename, apps=apps,**plot_arguments)
+                    
+                groups=request.form.getlist("vals")
+                for g in list(plot_arguments["groups_settings"].keys()):
+                    if g not in groups:
+                        del(plot_arguments["groups_settings"][g])
+
+                groups_settings=plot_arguments["groups_settings"]           
+                for group in groups:
+                    if group not in list(groups_settings.keys()):
+                        groups_settings[group]={"name":group,\
+                                "values":df[group].tolist(),\
+                                "label":group,\
+                                "color_value":None,\
+                                "color_rgb":"",\
+                                "bins_value":"auto",\
+                                "bins_number":"",\
+                                "orientation_value":"vertical",\
+                                "histtype_value":"bar",\
+                                "linewidth":0.5,\
+                                "linestyle_value":"solid",\
+                                "line_color":None,\
+                                "line_rgb":"",\
+                                "log_scale":".on",\
+                                "fill_alpha":0.8 }
+                        
+                    else:
+                        groups_settings[group]={"name":group,\
+                            "values":df[group].tolist(),\
+                            "label":request.form["%s.label" %group],\
+                            "color_value":request.form["%s.color_value" %group],\
+                            "color_rgb":request.form["%s.color_rgb" %group],\
+                            "bins_value":request.form["%s.bins_value" %group],\
+                            "bins_number":request.form["%s.bins_number" %group],\
+                            "orientation_value":request.form["%s.orientation_value" %group],\
+                            "histtype_value":request.form["%s.histtype_value" %group],\
+                            "linewidth":request.form["%s.linewidth" %group],\
+                            "linestyle_value":request.form["%s.linestyle_value" %group],\
+                            "line_color":request.form["%s.line_color" %group],\
+                            "line_rgb":request.form["%s.line_rgb" %group],\
+                            "fill_alpha":request.form["%s.fill_alpha" %group]}
+                            
+                        #Checkboxes are only present in request form if they are checked
+                        if group+".log_scale" in list(request.form.keys()):
+                            groups_settings[group]["log_scale"]=request.form["%s.log_scale" %group]
+                        else:
+                            groups_settings[group]["log_scale"]="off"
+
+
+            
+            plot_arguments["groups_settings"]=groups_settings
+            session["plot_arguments"]=plot_arguments
+            # MAKE SURE WE HAVE THE LATEST ARGUMENTS FOR THIS SESSION
+            filename=session["filename"]
+            fig=make_figure(df,plot_arguments)
+
+            #TRANSFORM FIGURE TO BYTES AND BASE64 STRING
+            figfile = io.BytesIO()
+            plt.savefig(figfile, format='png')
+            plt.close()
+            figfile.seek(0)  # rewind to beginning of file
+            figure_url = base64.b64encode(figfile.getvalue()).decode('utf-8')                
+            return render_template('/apps/histogram.html', figure_url=figure_url, filename=filename, apps=apps, **plot_arguments)
+                    
+        except Exception as e:
+            tb_str=handle_exception(e,user=current_user,eapp="histogram",session=session)
+            flash(tb_str,'traceback')
+            filename=session["filename"]
+            plot_arguments=session["plot_arguments"]
+            return render_template('/apps/histogram.html', filename=filename, apps=apps, **plot_arguments)
+
+    else:
+        if download == "download":
+            # READ INPUT DATA FROM SESSION JSON
+            df=pd.read_json(session["df"])
+
+            plot_arguments=session["plot_arguments"]
+
+            # CALL FIGURE FUNCTION
+            fig=make_figure(df,plot_arguments)
+
+            #flash('Figure is being sent to download but will not be updated on your screen.')
+            figfile = io.BytesIO()
+            mimetypes={"png":'image/png',"pdf":"application/pdf","svg":"image/svg+xml"}
+            plt.savefig(figfile, format=plot_arguments["downloadf"])
+            plt.close()
+            figfile.seek(0)  # rewind to beginning of file
+
+            eventlog = UserLogging(email=current_user.email,action="download figure histogram")
+            db.session.add(eventlog)
+            db.session.commit()
+
+            return send_file(figfile, mimetype=mimetypes[plot_arguments["downloadf"]], as_attachment=True, attachment_filename=plot_arguments["downloadn"]+"."+plot_arguments["downloadf"] )
+        
+        return render_template('apps/histogram.html',  filename=session["filename"], apps=apps, **session["plot_arguments"])
