ac3d86397745be3dada3422342c6c9e9d33db653
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2020-08-20 10:55:07 +0200

    prevent Chrome caching, closes #130

--- flaski/userspace.py
+++ flaski/userspace.py
@@ -20,6 +20,17 @@ from flaski.routes import FREEAPPS
 from flaski.routines import session_to_file
 from flaski import app, sess
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+
 @app.route('/userspace', methods=['GET', 'POST'])
 @login_required
 def userspace():
