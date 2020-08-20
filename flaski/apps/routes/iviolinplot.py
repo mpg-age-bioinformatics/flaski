5041f221865c9411777d99834997d8c81081f2d4
Camila Duitama <camiladuitama@gmail.com>
2020-08-20 13:59:29 +0300

    add_header function added

--- flaski/apps/routes/iviolinplot.py
+++ flaski/apps/routes/iviolinplot.py
@@ -24,7 +24,18 @@ import json
 import pandas as pd
 
 import base64
-
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+    
 @app.route('/iviolinplot/<download>', methods=['GET', 'POST'])
 @app.route('/iviolinplot', methods=['GET', 'POST'])
 @login_required
--- flaski/apps/routes/violinplot.py
+++ flaski/apps/routes/violinplot.py
@@ -30,6 +30,18 @@ import pandas as pd
 
 import base64
 
+@app.after_request
+def add_header(r):
+    """
+     Add headers to both force latest IE rendering engine or Chrome Frame,
+     and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
+
 @app.route('/violinplot/<download>', methods=['GET', 'POST'])
 @app.route('/violinplot', methods=['GET', 'POST'])
 @login_required
