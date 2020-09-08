e27d3a547fa4fadfcc0a72643e5cc9801b6a9ec9
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2020-09-08 15:00:41 +0200

    kegg: started #93

--- utils/dev.py
+++ utils/dev.py
@@ -1,5 +1,6 @@
-from flaski.apps.routes import ihistogram, lifespan, idendrogram 
+from flaski.apps.routes import ihistogram, lifespan, idendrogram, kegg
 
 EXTERNAL_APPS=[{ "name":"iHistogram", "id":'ihistogram_more',"link":'ihistogram' ,"java":"javascript:ReverseDisplay('iHistogram_more')", "description":"An interactive Histogram app."},\
             { "name":"LifeSpan", "id":'lifespan_more',"link":'lifespan' ,"java":"javascript:ReverseDisplay('lifespan_more')", "description":"A Survival Analysis app."},\
-            { "name":"iDendrogram", "id":'idendrogram',"link":'idendrogram' ,"java":"javascript:ReverseDisplay('idendrogram_more')", "description":"An interactive Dendrogram app."}] #,\ #,\
+            { "name":"iDendrogram", "id":'idendrogram_more',"link":'idendrogram' ,"java":"javascript:ReverseDisplay('idendrogram_more')", "description":"An interactive Dendrogram app."},\
+            {"name":"KEGG", "id":'kegg_more',"link":'kegg' ,"java":"javascript:ReverseDisplay('kegg_more')", "description":"A KEGG mapping and plotting app."}] #,\ #,\
