84a80238ec0e4f39ee4197dd0af532b927d9aff2
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-02-22 15:21:08 +0100

    submissions

---
+++ utils/external.dev.py
@@ -0,0 +1,16 @@
+###########################################################################
+#
+# example of Apps imported as plugin through docker-compose.yml mapping
+#
+# external.py (this file):
+# from flaski.apps.routes import histogram
+# EXTERNAL_APPS=[{ "name":"Histogram", "id":'histogram_more',"link":'histogram' ,"java":"javascript:ReverseDisplay('histogram_more')", "description":"A histogram."}]
+#
+# docker-compose.yml:
+#   volumes:
+#    - ~/histogram/route.py:/flaski/flaski/apps/routes/histogram.py
+#    - ~/histogram/main.py:/flaski/flaski/apps/main/histogram.py
+#    - ~/histogram/external.py:/flaski/flaski/apps/external.py
+#
+###########################################################################
+EXTERNAL_APPS=[{ "name":"Submissions", "id":'submissions_more',"link":'submissions' ,"java":"javascript:ReverseDisplay('submissions_more')", "description":"Samples submissions."}]
