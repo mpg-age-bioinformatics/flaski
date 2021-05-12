5191d7bd463f087815b32f34fe7d6124dd956d73
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-05-12 15:21:36 +0200

    submissions: variantCalling

--- flaski/apps/main/submissions.py
+++ flaski/apps/main/submissions.py
@@ -91,11 +91,46 @@ def check_intronret(EXC):
 
   return status, msg, filename
 
+def check_variantCalling(EXC):
+  status=False
+  attachment_path=None
+  if "samples" not in EXC.sheet_names:
+    status=False
+    msg="Could not find sample information - 'samples' sheet - in the submission file."
+    return status, msg, attachment_path
+  metadata=EXC.parse("variantCalling")
+  email=metadata[  metadata["Field"] == "email"][ "Value" ].values[0]
+  email=str(email).rstrip().lstrip()
+  email=email.split(",")
+  email=[ re.search("([^@|\s]+@[^@]+\.[^@|\s]+)",e,re.I) for e in email ]
+  email=[ e.group(1) for e in email if e ]
+  if not email :
+    msg="Contact email is not a valid email. Please provide a valid email in the 'email' field of your submission file."
+    return status, msg, attachment_path
+  nas=metadata[metadata["Value"].isna()]["Field"].tolist()
+  if nas:
+    msg="The following fields require a valid value: {fields} ".format(fields=", ".join(nas) )
+    return status, msg, attachment_path
+  
+  status="variantCalling"
+  msg="Submission successuful. Please check for email confirmation."
+
+  filename=make_submission_file(".variantCalling.xlsx")
+  # print(os.readlink(filename))
+
+  EXCout=pd.ExcelWriter(filename)
+  metadata.to_excel(EXCout,"variantCalling",index=None)
+  samples=EXC.parse("samples")
+  samples.to_excel(EXCout,"samples",index=None)
+  EXCout.save()
+
+  return status, msg, filename
+
 def submission_check(inputfile):
   status=False
   attachment_path=None
 
-  valid_submissions=["RNAseq", "intronRet" ]
+  valid_submissions=["RNAseq", "intronRet", "variantCalling" ]
 
   filename = secure_filename(inputfile.filename)
   fileread = inputfile.read()
@@ -120,6 +155,9 @@ def submission_check(inputfile):
     status, msg, attachment_path=check_rnaseq(EXC)
   elif submission_type[0]=="intronRet":
     status, msg, attachment_path=check_intronret(EXC)
+  elif submission_type[0]=="variantCalling":
+    status, msg, attachment_path=check_variantCalling(EXC)
+    
   else:
     msg="Submission failed."
 
