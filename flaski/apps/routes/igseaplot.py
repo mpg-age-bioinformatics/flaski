5c1b1c821711bdfa74f654ccfb7a78d61169c538
FranziMe <f.metge88@gmail.com>
2021-07-06 15:23:22 +0200

    #176 igsea start

--- flaski/apps/routes/igseaplot.py
+++ flaski/apps/routes/igseaplot.py
@@ -7,7 +7,7 @@ from flask_login import current_user, login_user, logout_user, login_required
 from datetime import datetime
 from flaski import db
 from werkzeug.urls import url_parse
-from flaski.apps.main.igsea_plot import make_figure, figure_defaults
+from flaski.apps.main.igseaplot import make_figure, figure_defaults
 from flaski.models import User, UserLogging
 from flaski.routines import session_to_file, check_session_app, handle_exception, read_request, read_tables, allowed_file, read_argument_file, read_session_file
 from flaski.email import send_exception_email
@@ -37,10 +37,10 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
-@app.route('/igsea_plot/<download>', methods=['GET', 'POST'])
-@app.route('/igsea_plot', methods=['GET', 'POST'])
+@app.route('/igseaplot/<download>', methods=['GET', 'POST'])
+@app.route('/igseaplot', methods=['GET', 'POST'])
 @login_required
-def igsea_plot(download=None):
+def igseaplot(download=None):
     """ 
     renders the plot on the fly.
     https://gist.github.com/illume/1f19a2cf9f26425b1761b63d9506331f
@@ -48,7 +48,7 @@ def igsea_plot(download=None):
     apps=current_user.user_apps
     plot_arguments=None  
 
-    reset_info=check_session_app(session,"igsea_plot",apps)
+    reset_info=check_session_app(session,"igseaplot",apps)
     if reset_info:
         flash(reset_info,'error')
 
@@ -57,7 +57,7 @@ def igsea_plot(download=None):
         plot_arguments=figure_defaults()
         session["plot_arguments"]=plot_arguments
         session["COMMIT"]=app.config['COMMIT']
-        session["app"]="igsea_plot"
+        session["app"]="igseaplot"
 
     if request.method == 'POST' :
 
@@ -65,17 +65,17 @@ def igsea_plot(download=None):
             # READ SESSION FILE IF AVAILABLE 
             # AND OVERWRITE VARIABLES
             if request.files["inputsessionfile"] :
-                msg, plot_arguments, error=read_session_file(request.files["inputsessionfile"],"igsea_plot")
+                msg, plot_arguments, error=read_session_file(request.files["inputsessionfile"],"igseaplot")
                 if error:
                     flash(msg,'error')
-                    return render_template('/apps/igsea_plot.html' , filename=session["filename"],apps=apps, **plot_arguments)
+                    return render_template('/apps/igseaplot.html' , filename=session["filename"],apps=apps, **plot_arguments)
                 flash(msg,"info")
 
             if request.files["inputargumentsfile"] :
-                msg, plot_arguments, error=read_argument_file(request.files["inputargumentsfile"],"igsea_plot")
+                msg, plot_arguments, error=read_argument_file(request.files["inputargumentsfile"],"igseaplot")
                 if error:
                     flash(msg,'error')
-                    return render_template('/apps/igsea_plot.html' , filename=session["filename"], apps=apps, **plot_arguments)
+                    return render_template('/apps/igseaplot.html' , filename=session["filename"], apps=apps, **plot_arguments)
                 flash(msg,"info")
             
             # IF THE UPLOADS A NEW FILE 
@@ -92,7 +92,7 @@ def igsea_plot(download=None):
                     if len(cols) < 2 :
                         error_msg="Your table needs to have at least 2 columns. One for the x- and one for the y-value."
                         flash(error_msg,'error')
-                        return render_template('/apps/igsea_plot.html' , filename=session["filename"], apps=apps, **plot_arguments)
+                        return render_template('/apps/igseaplot.html' , filename=session["filename"], apps=apps, **plot_arguments)
 
                     if session["plot_arguments"]["groups"] not in cols:
                         session["plot_arguments"]["groups"]=["None"]+cols
@@ -115,18 +115,18 @@ def igsea_plot(download=None):
                         sometext="Please select which values should map to the x and y axes."
                         plot_arguments=session["plot_arguments"]
                         flash(sometext,'info')
-                        return render_template('/apps/igsea_plot.html' , filename=filename, apps=apps,**plot_arguments)
+                        return render_template('/apps/igseaplot.html' , filename=filename, apps=apps,**plot_arguments)
                         
                     plot_arguments=session["plot_arguments"]
                     flash("New file uploaded.",'info')
-                    return render_template('/apps/igsea_plot.html' , filename=filename, apps=apps,**plot_arguments)
+                    return render_template('/apps/igseaplot.html' , filename=filename, apps=apps,**plot_arguments)
 
                 else:
                     # IF UPLOADED FILE DOES NOT CONTAIN A VALID EXTENSION PLEASE UPDATE
                     error_msg="You can can only upload files with the following extensions: 'xlsx', 'tsv', 'csv'. Please make sure the file '%s' \
                     has the correct format and respective extension and try uploadling it again." %filename
                     flash(error_msg,'error')
-                    return render_template('/apps/igsea_plot.html' , filename="Select file..", apps=apps, **plot_arguments)
+                    return render_template('/apps/igseaplot.html' , filename="Select file..", apps=apps, **plot_arguments)
             
             if not request.files["inputsessionfile"] and not request.files["inputargumentsfile"] :
                 # SELECTION LISTS DO NOT GET UPDATED 
@@ -206,7 +206,7 @@ def igsea_plot(download=None):
             if "df" not in list(session.keys()):
                 error_msg="No data to plot, please upload a data or session  file."
                 flash(error_msg,'error')
-                return render_template('/apps/igsea_plot.html' , filename="Select file..", apps=apps,  **plot_arguments)
+                return render_template('/apps/igseaplot.html' , filename="Select file..", apps=apps,  **plot_arguments)
 
             # MAKE SURE WE HAVE THE LATEST ARGUMENTS FOR THIS SESSION
             filename=session["filename"]
@@ -218,15 +218,15 @@ def igsea_plot(download=None):
             #CALL FIGURE FUNCTION
             fig=make_figure(df,plot_arguments)
             figure_url = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-            return render_template('/apps/igsea_plot.html', figure_url=figure_url, filename=filename, apps=apps, **plot_arguments)
+            return render_template('/apps/igseaplot.html', figure_url=figure_url, filename=filename, apps=apps, **plot_arguments)
 
         except Exception as e:
-            tb_str=handle_exception(e,user=current_user,eapp="igsea_plot",session=session)
+            tb_str=handle_exception(e,user=current_user,eapp="igseaplot",session=session)
             flash(tb_str,'traceback')
             if not plot_arguments:
                 plot_arguments=session["plot_arguments"]
             filename=session["filename"]
-            return render_template('/apps/igsea_plot.html', filename=filename, apps=apps, **plot_arguments)
+            return render_template('/apps/igseaplot.html', filename=filename, apps=apps, **plot_arguments)
 
     else:
         if download == "download":
@@ -259,10 +259,10 @@ def igsea_plot(download=None):
 
             figfile.seek(0)  # rewind to beginning of file
 
-            eventlog = UserLogging(email=current_user.email,action="download figure igsea_plot")
+            eventlog = UserLogging(email=current_user.email,action="download figure igseaplot")
             db.session.add(eventlog)
             db.session.commit()
 
             return send_file(figfile, mimetype=mimetypes[plot_arguments["downloadf"]], as_attachment=True, attachment_filename=plot_arguments["downloadn"]+"."+plot_arguments["downloadf"] )
         
-        return render_template('apps/igsea_plot.html',  filename=session["filename"], apps=apps, **session["plot_arguments"])
+        return render_template('apps/igseaplot.html',  filename=session["filename"], apps=apps, **session["plot_arguments"])
