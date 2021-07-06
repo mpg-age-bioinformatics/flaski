1c4afd260144b9e227dc10f68899fa24c0ed7598
FranziMe <f.metge88@gmail.com>
2021-07-06 16:07:22 +0200

    #176 igsea dev

--- utils/dev.py
+++ utils/dev.py
@@ -1,2 +1,6 @@
-EXTERNAL_APPS=[]
-EXT_DEFAULTS_DIC={}
+from flaski.apps.routes import igseaplot
+ 
+EXTERNAL_APPS=[{ "name":"iGSEA plot", "id":'igseaplot_more',"link":'igseaplot' ,"java":"javascript:ReverseDisplay('igseaplot_more')", "description":"An app to customize GSEA plots."}]
+
+from flaski.apps.main.igseaplot import figure_defaults as igseaplot_def
+EXT_DEFAULTS_DIC={"igseaplot":igseaplot_def}
