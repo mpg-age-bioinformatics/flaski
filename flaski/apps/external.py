1f9448e7124624f783c55487256cff86f108b43c
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-07-14 18:26:07 +0200

    making external once more with proper commented examples

--- flaski/apps/external.py
+++ flaski/apps/external.py
@@ -17,15 +17,15 @@
 #    - ~/histogram/external.py:/flaski/flaski/apps/external.py
 #
 ###########################################################################
-from flaski.apps.routes import igseaplot
+# from flaski.apps.routes import igseaplot
  
-EXTERNAL_APPS=[{ "name":"iGSEA plot", "id":'igseaplot_more',"link":'igseaplot' ,"java":"javascript:ReverseDisplay('igseaplot_more')", "description":"An app to customize GSEA plots."}]
+# EXTERNAL_APPS=[{ "name":"iGSEA plot", "id":'igseaplot_more',"link":'igseaplot' ,"java":"javascript:ReverseDisplay('igseaplot_more')", "description":"An app to customize GSEA plots."}]
 
-from flaski.apps.main.igseaplot import figure_defaults as igseaplot_def
-EXT_DEFAULTS_DIC={"igseaplot":igseaplot_def}
+# from flaski.apps.main.igseaplot import figure_defaults as igseaplot_def
+# EXT_DEFAULTS_DIC={"igseaplot":igseaplot_def}
 
-docker-compose.yml:
-    volumes:
-        - ~/histogram/route.py:/flaski/flaski/apps/routes/igseaplot.py
-        - ~/histogram/main.py:/flaski/flaski/apps/main/igseaplot.py
-        - ~/histogram/external.py:/flaski/flaski/apps/igseaplot.py
+# docker-compose.yml:
+#     volumes:
+#         - ~/histogram/route.py:/flaski/flaski/apps/routes/igseaplot.py
+#         - ~/histogram/main.py:/flaski/flaski/apps/main/igseaplot.py
+#         - ~/histogram/external.py:/flaski/flaski/apps/igseaplot.py
