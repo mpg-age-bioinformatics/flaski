949e3b723b7d0eeba731435faf4ff93a71308bd9
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2021-10-11 12:20:07 +0200

    added protected view

--- routes/apps/scatterplot.py
+++ routes/apps/scatterplot.py
@@ -1,10 +1,12 @@
 from myapp import app
 import dash
-from myapp.routes._utils import META_TAGS
+from myapp.routes._utils import META_TAGS, protect_dashviews
 import dash_bootstrap_components as dbc
 from dash import dcc, html
 
 dashapp = dash.Dash("scatterplot",url_base_pathname='/scatterplot/', meta_tags=META_TAGS, server=app, external_stylesheets=[dbc.themes.BOOTSTRAP], title=app.config["APP_TITLE"], assets_folder=app.config["APP_ASSETS"])# , assets_folder="/flaski/flaski/static/dash/")
+
+protect_dashviews(dashapp)
 
 dashapp.layout=html.Div( 
     [ 
