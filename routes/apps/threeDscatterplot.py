90e73f24150806280e3393a96e77c22fdd4a2a02
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-05-02 15:47:09 +0200

    cache bug fix

--- routes/apps/threeDscatterplot.py
+++ routes/apps/threeDscatterplot.py
@@ -39,7 +39,7 @@ elif app.config["CACHE_TYPE"] == "RedisSentinelCache" :
     cache = Cache(dashapp.server, config={
         'CACHE_TYPE': 'RedisSentinelCache',
         'CACHE_REDIS_SENTINELS': [ 
-            [ (os.environ.get('CACHE_REDIS_SENTINELS_address'), os.environ.get('CACHE_REDIS_SENTINELS_port')) ]
+            [ os.environ.get('CACHE_REDIS_SENTINELS_address'), os.environ.get('CACHE_REDIS_SENTINELS_port') ]
         ],
         'CACHE_REDIS_SENTINEL_MASTER': os.environ.get('CACHE_REDIS_SENTINEL_MASTER')
     })
