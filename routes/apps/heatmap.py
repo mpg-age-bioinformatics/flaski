986418693898779c53d551992a480b73c93191d1
Ayesha Iqbal <aiqbal@age.mpg.de>
2022-07-04 16:30:52 +0200

    heatmaps clusters downloads

--- routes/apps/heatmap.py
+++ routes/apps/heatmap.py
@@ -957,7 +957,25 @@ def make_app_content(pathname):
                                         ],
                                         id="download-pdf-div",
                                         style={"max-width":"150px","width":"100%","margin":"4px", 'display': 'none'} # 'none' / 'inline-block'
-                                    )
+                                    ),
+                                    html.Div( 
+                                        [
+                                            dbc.Button(
+                                                html.Span(
+                                                    [ 
+                                                        html.I(className="far fa-lg fa-save"),
+                                                        " Results (xlsx)" 
+                                                    ]
+                                                ),
+                                                id='save-excel-btn', 
+                                                style={"max-width":"150px","width":"100%"},
+                                                color="secondary"
+                                            ),
+                                            dcc.Download(id='save-excel')
+                                        ],
+                                        id="save-excel-div",
+                                        style={"max-width":"150px","width":"100%","margin":"4px", 'display':'none'} # 'none' / 'inline-block'
+                                    ),                                    
                                 ],
                                 style={"height":"100%"}
                             ),
@@ -1016,7 +1034,26 @@ def make_app_content(pathname):
                         ],
                         id="export-filename-modal",
                         is_open=False,
-                    )
+                    ),
+
+                    dbc.Modal(
+                        [
+                            dbc.ModalHeader("File name"), # dbc.ModalTitle(
+                            dbc.ModalBody(
+                                [
+                                    dcc.Input(id='excel-filename', value="clusters.xlsx", type='text', style={"width":"100%"})
+                                ]
+                            ),
+                            dbc.ModalFooter(
+                                dbc.Button(
+                                    "Download", id="excel-filename-download", className="ms-auto", n_clicks=0
+                                )
+                            ),
+                        ],
+                        id="excel-filename-modal",
+                        is_open=False,
+                    ),
+
                 ],
             align="start",
             justify="left",
@@ -1188,21 +1225,25 @@ states=[State('xvals', 'value'),
     Output( 'toast-make_fig_output','children'),
     Output('session-data','data'),
     Output({ "type":"traceback", "index":"make_fig_output" },'data'),
+    Output('save-excel-div', 'style'),
     Output('download-pdf-div', 'style'),
     Output('export-session','data'),
+    Output('save-excel', 'data'),
     Input("submit-button-state", "n_clicks"),
     Input("export-filename-download","n_clicks"),
     Input("save-session-btn","n_clicks"),
     Input("saveas-session-btn","n_clicks"),
+    Input("excel-filename-download","n_clicks"),
     [ State('session-id', 'data'),
     State('upload-data', 'contents'),
     State('upload-data', 'filename'),
     State('upload-data', 'last_modified'),
     State('export-filename','value'),
+    State('excel-filename', 'value'),
     State('upload-data-text', 'children')] + states,
     prevent_initial_call=True
     )
-def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,session_id,contents,filename,last_modified,export_filename,upload_data_text, *args):
+def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,save_excel_btn,session_id,contents,filename,last_modified,export_filename,excel_filename,upload_data_text, *args):
     ## This function can be used for the export, save, and save as by making use of 
     ## Determining which Input has fired with dash.callback_context
     ## in https://dash.plotly.com/advanced-callbacks
@@ -1222,14 +1263,19 @@ def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,se
         for k, a in zip(input_names,args) :
             if type(k) != dict :
                 pa[k]=a
+            elif type(k) == dict :
+                k_=k['type'] 
+                for i, a_ in enumerate(a) :
+                    pa[k_]=a_
 
-        session_data={ "session_data": {"app": { "heatmap": {"filename":upload_data_text ,'last_modified':last_modified,"df":df.to_json(),"pa":pa} } } }
+
+        session_data={ "session_data": {"app": { "heatmap": {"filename":upload_data_text, "last_modified":last_modified,"df":df.to_json(),"pa":pa} } } }
         session_data["APP_VERSION"]=app.config['APP_VERSION']
         
     except Exception as e:
         tb_str=''.join(traceback.format_exception(None, e, e.__traceback__))
         toast=make_except_toast("There was a problem parsing your input.","make_fig_output", e, current_user,"heatmap")
-        return dash.no_update, toast, None, tb_str, download_buttons_style_hide, None
+        return dash.no_update, toast, None, tb_str, download_buttons_style_hide, download_buttons_style_hide, None, None
 
     # button_id,  submit-button-state, export-filename-download
 
@@ -1244,7 +1290,34 @@ def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,se
             export_filename.write(json.dumps(session_data).encode())
             # export_filename.seek(0)
 
-        return dash.no_update, None, None, None, dash.no_update, dcc.send_bytes(write_json, export_filename)
+        return dash.no_update, None, None, None, dash.no_update, dash.no_update, dcc.send_bytes(write_json, export_filename), None
+
+    if button_id == "excel-filename-download":
+
+        eventlog = UserLogging(email=current_user.email,action="download clusters")
+        db.session.add(eventlog)
+        db.session.commit()
+
+        if not excel_filename:
+            excel_filename=secure_filename("clusters.%s.xlsx" %(time.strftime("%Y%m%d_%H%M%S", time.localtime())))
+        excel_filename=secure_filename(excel_filename)
+        if excel_filename.split(".")[-1] != "xlsx":
+            excel_filename=f'{excel_filename}.xlsx'  
+
+        fig, clusters_cols, clusters_rows, df_=make_figure(df,pa)
+
+        import io
+        output = io.BytesIO()
+        writer= pd.ExcelWriter(output)
+
+        df_.to_excel(writer, sheet_name = 'heatmap', index = None)
+        if type(clusters_cols) != type(None):
+            clusters_cols.to_excel(writer,sheet_name="rows",index=None)
+        if type(clusters_rows) != type(None):
+            clusters_rows.to_excel(writer,sheet_name="cols",index=None)
+        writer.save()
+        data=output.getvalue()
+        return dash.no_update, None, None, None, dash.no_update,dash.no_update, dash.no_update, dcc.send_bytes(data, excel_filename)
 
     if button_id == "save-session-btn" :
         try:
@@ -1253,24 +1326,28 @@ def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,se
                 return dash.no_update, toast, None, None, dash.no_update, None
             else:
                 session["session_data"]=session_data
-                return dcc.Location(pathname=f"{PAGE_PREFIX}/storage/saveas/", id='index'), dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+                return dcc.Location(pathname=f"{PAGE_PREFIX}/storage/saveas/", id='index'), dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
                 # save session_data to redis session
                 # redirect to as a save as to file server
 
         except Exception as e:
             tb_str=''.join(traceback.format_exception(None, e, e.__traceback__))
             toast=make_except_toast("There was a problem saving your file.","save", e, current_user,"heatmap")
-            return dash.no_update, toast, None, tb_str, dash.no_update, None
+            return dash.no_update, toast, None, tb_str, dash.no_update, dash.no_update, None, None
 
         # return dash.no_update, None, None, None, dash.no_update, dcc.send_bytes(write_json, export_filename)
 
     if button_id == "saveas-session-btn" :
         session["session_data"]=session_data
-        return dcc.Location(pathname=f"{PAGE_PREFIX}/storage/saveas/", id='index'), dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
+        return dcc.Location(pathname=f"{PAGE_PREFIX}/storage/saveas/", id='index'), dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update, dash.no_update
           # return dash.no_update, None, None, None, dash.no_update, dcc.send_bytes(write_json, export_filename)
     
     try:
         fig, clusters_cols, clusters_rows, df_=make_figure(df,pa)
+        print(clusters_cols)
+        print(clusters_rows)
+        print(df_)
+        print(pa)
         # import plotly.graph_objects as go
         # fig = go.Figure( )
         # fig.update_layout( )
@@ -1282,18 +1359,18 @@ def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,se
         #             'yanchor': 'top' ,
         #             "font": {"size": 25, "color":"black"  } } )
         fig_config={ 'modeBarButtonsToRemove':["toImage"], 'displaylogo': False}
-        fig=dcc.Graph(figure=fig,config=fig_config,  id="graph", responsive=True)
+        fig=dcc.Graph(figure=fig,config=fig_config,  id="graph")#, responsive=True)
 
         # changed
         # return fig, None, session_data, None, download_buttons_style_show
         # as session data is no longer required for downloading the figure
 
-        return fig, None, None, None, download_buttons_style_show, None
+        return fig, None, None, None, download_buttons_style_show, download_buttons_style_show, None, None
         
     except Exception as e:
         tb_str=''.join(traceback.format_exception(None, e, e.__traceback__))
         toast=make_except_toast("There was a problem generating your output.","make_fig_output", e, current_user,"heatmap")
-        return dash.no_update, toast, session_data, tb_str, download_buttons_style_hide, None
+        return dash.no_update, toast, session_data, tb_str, download_buttons_style_hide, download_buttons_style_hide, None, None
 
 @dashapp.callback(
     Output('export-filename-modal', 'is_open'),
@@ -1302,6 +1379,17 @@ def make_fig_output(n_clicks,export_click,save_session_btn,saveas_session_btn,se
     prevent_initial_call=True
 )
 def download_export_filename(n1, n2, is_open):
+    if n1 or n2:
+        return not is_open
+    return is_open
+
+@dashapp.callback(
+    Output('excel-filename-modal', 'is_open'),
+    [ Input('save-excel-btn',"n_clicks"),Input("excel-filename-download", "n_clicks")],
+    [ State("excel-filename-modal", "is_open")], 
+    prevent_initial_call=True
+)
+def download_excel_filename(n1, n2, is_open):
     if n1 or n2:
         return not is_open
     return is_open
