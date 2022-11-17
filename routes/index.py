a171b3a8d8644317a0dd34d56dd406e92ff63316
Jorge Boucas <Jorge.Boucas@age.mpg.de>
2022-11-17 14:01:44 +0100

    metatags

--- routes/index.py
+++ routes/index.py
@@ -15,8 +15,6 @@ import base64
 import pandas as pd
 import os
 
-META_TAGS=[{'name': 'viewport', 'content': 'width=device-width, initial-scale=1.0, maximum-scale=1.2, minimum-scale=0.5,'} ]
-
 dashapp = dash.Dash("index", url_base_pathname=f"{PAGE_PREFIX}/", meta_tags=META_TAGS, server=app, external_stylesheets=[dbc.themes.BOOTSTRAP], title=app.config["APP_TITLE"], assets_folder=app.config["APP_ASSETS"])# , assets_folder="/flaski/flaski/static/dash/")
 
 # protect_dashviews(dashapp)
