fb224cb601592f8cc54d2f7ca2c9b9fefd4d5a22
Jorge Boucas <jboucas@age.mpg.de>
2023-10-04 15:56:07 +0200

    rnaseq: cytoscape for raven removed from production

--- routes/apps/rnaseq.py
+++ routes/apps/rnaseq.py
@@ -323,8 +323,7 @@ def generate_submission_file(rows, email,group,folder,md5sums,project_title,orga
                 "circRNA":"None",
             },
             "raven":{
-                "cytoscape_host":"/nexus/posix0/MAGE-flaski/service/projects/code/Bioinformatics/bit_mpcdf_automation/cytoscape.ip.txt",
-                "cytoscape_ip_mount":"-B /nexus/posix0/MAGE-flaski/service/projects/code/Bioinformatics/bit_mpcdf_automation/cytoscape.ip.txt_inuse:/cytoscape.ip.txt",
+                "cytoscape_ip_mount":"",
                 "homefolder":"", 
                 "project_folder" : os.path.join(paths["raven"]["run_data"], project_folder) ,
                 "samplestable":os.path.join(paths["raven"]["code"], project_folder, "scripts.jboucas" ,"sample_sheet.xlsx"),
