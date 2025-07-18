2ba06c05138486b48269e1ec5ac38473da041fe5
sourovamin <sourov.amin@gmail.com>
2025-07-18 13:30:43 +0200

    removed unsuccessful chnages

--- routes/apps/david.py
+++ routes/apps/david.py
@@ -50,8 +50,7 @@ elif app.config["CACHE_TYPE"] == "RedisSentinelCache" :
         'CACHE_REDIS_SENTINELS': [ 
             [ os.environ.get('CACHE_REDIS_SENTINELS_address'), os.environ.get('CACHE_REDIS_SENTINELS_port') ]
         ],
-        'CACHE_REDIS_SENTINEL_MASTER': os.environ.get('CACHE_REDIS_SENTINEL_MASTER'),
-        'CACHE_REDIS_SENTINEL_READ_FROM_SLAVE': False
+        'CACHE_REDIS_SENTINEL_MASTER': os.environ.get('CACHE_REDIS_SENTINEL_MASTER')
     })
 
 def run_david_and_cache(pa,cache):
