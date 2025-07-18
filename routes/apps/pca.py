81cdd60541a43a1ffeed18ed58ab2c3ce631675a
sourovamin <sourov.amin@gmail.com>
2025-07-18 11:56:58 +0200

    to prevent non-critical redis error alongside primary error

--- routes/apps/pca.py
+++ routes/apps/pca.py
@@ -49,7 +49,8 @@ elif app.config["CACHE_TYPE"] == "RedisSentinelCache" :
         'CACHE_REDIS_SENTINELS': [ 
             [ os.environ.get('CACHE_REDIS_SENTINELS_address'), os.environ.get('CACHE_REDIS_SENTINELS_port') ]
         ],
-        'CACHE_REDIS_SENTINEL_MASTER': os.environ.get('CACHE_REDIS_SENTINEL_MASTER')
+        'CACHE_REDIS_SENTINEL_MASTER': os.environ.get('CACHE_REDIS_SENTINEL_MASTER'),
+        'CACHE_REDIS_SENTINEL_READ_FROM_SLAVE': False
     })
 
 def run_pca_and_cache(df, pa, cache):
